af5cf4e0b569c7c8225c099217d30c7ac38b55ed
iamrahulpanchal <rahulnpanchal50@gmail.com>
2020-09-30 14:22:18 +0530

    Added Error Handling if no document

--- mongodb.js
+++ mongodb.js
@@ -12,9 +12,12 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName);
 
-    db.collection('users').findOne({ name: 'Bhavin' }, (error, user) => {
+    db.collection('users').findOne({ name: 'Bhavin', age: 1 }, (error, user) => {
         if(error){
             return console.log(`Unable to fetch user`);
+        }
+        if(!user){
+            return console.log(`User Not Found`);
         }
         console.log(user);
     });
