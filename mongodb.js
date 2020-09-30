93f65aca339e24ac3dc4bccf6ced62c5e17cee78
iamrahulpanchal <rahulnpanchal50@gmail.com>
2020-09-30 12:29:19 +0530

    Insert One Collection

--- mongodb.js
+++ mongodb.js
@@ -2,11 +2,17 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
-const database = 'task-manager';
+const databaseName = 'task-manager';
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if(error){
         return console.log(`Unable to Connect`);
     }
     console.log(`Connected to DB`);
+
+    const db = client.db(databaseName);
+    db.collection('users').insertOne({
+        name: 'Rahul Panchal',
+        age: 24
+    });
 });
