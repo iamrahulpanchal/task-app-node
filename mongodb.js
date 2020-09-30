b3fe97b2a1f9bcebf3f06be78e62637d808761b6
iamrahulpanchal <rahulnpanchal50@gmail.com>
2020-09-30 14:20:34 +0530

    Reading One Document from DB

--- mongodb.js
+++ mongodb.js
@@ -1,13 +1,8 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
-const ObjectID = mongodb.ObjectID;
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
-
-const id = new ObjectID();
-console.log(id);
-// console.log(id.getTimestamp());
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if(error){
@@ -17,52 +12,10 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-        _id: id,
-        name: 'Vikram',
-        age: 25
-    }, (error, result) => {
-        if(error) {
-            return console.log(`Unable to Insert`);
+    db.collection('users').findOne({ name: 'Bhavin' }, (error, user) => {
+        if(error){
+            return console.log(`Unable to fetch user`);
         }
-        console.log(result.ops);
-        console.log(result.insertedCount);
-    })
-
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Rahul',
-    //         age: 24
-    //     }, {
-    //         name: 'Bhavin',
-    //         age: 18
-    //     }, {
-    //         name: 'Mike',
-    //         age: 32
-    //     }
-    // ], (error, result) => {
-    //     if(error) {
-    //         return console.log(`Unable to Insert`);
-    //     }
-    //     console.log(result.ops);
-    //     console.log(result.insertedCount);
-    // });
-
-    // db.collection('tasks').insertMany([{
-    //     description: 'Task 1',
-    //     completed: true
-    // }, {
-    //     description: 'Task 2',
-    //     completed: false
-    // }, {
-    //     description: 'Task 3',
-    //     completed: false
-    // }], (error, result) => {
-    //     if(error) {
-    //         return console.log(`Unable to Insert`);
-    //     }
-    //     console.log(result.ops);
-    //     console.log(result.insertedCount);
-    // });
-
+        console.log(user);
+    });
 });
