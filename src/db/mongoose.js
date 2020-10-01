4eef903b434884208fed3eb8c5fdfa06fbd5fe39
iamrahulpanchal <rahulnpanchal50@gmail.com>
2020-10-01 14:57:38 +0530

    Creating User through Postman & Testing

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', { 
     useNewUrlParser: true, 
@@ -7,61 +6,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useUnifiedTopology: true 
 });
 
-// Creating a Collection
-const Users = mongoose.model('Users', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    age: {
-        type: Number,
-        required: true,
-        default: 0,
-        validate(value) {
-            if(value < 0){
-                throw new Error (`Age must be a Positive Number`);
-            }
-        }
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error `Email ID is not Valid!`;
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        trim: true,
-        validate(value){
-            if(value.toLowerCase().includes('password')){
-                throw new Error (`Password should not contain Password`);
-            }
-        }
-    }
-});
-
-// // Adding Data into Collection
-// const me = new Users({
-//     name: 'Testing Password',
-//     age: 25,
-//     email: 'rahul@gmail.com',
-//     password: 'rahulpanchal'
-// });
-
-// // Saving Data into DB
-// me.save().then((me) => {
-//     console.log(me);
-// }).catch((err) => {
-//     console.log(err);
-// });
 
 const Tasks = mongoose.model('Tasks', {
     description: {
@@ -73,14 +17,4 @@ const Tasks = mongoose.model('Tasks', {
         type: Boolean,
         default: false
     }
-});
-
-const task = new Tasks({
-    description: '          Description 111          ',
-});
-
-task.save().then((task) => {
-    console.log(task);
-}).catch((err) => {
-    console.log(err);
 });
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+require('./db/mongoose');   // For Connecting to DB
+const Users = require('./models/users');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send(`Hello`);
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+    const user = new Users(req.body);
+    user.save().then(() => {
+        res.send(user);
+    }).catch((err) => {
+        res.status(400).send(err);
+        // res.send(err);
+    })
 });
 
 app.listen(port, () =>{
