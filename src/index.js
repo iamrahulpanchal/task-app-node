2e82689c80e3bdba2fd80040bc89f12dc79eccef
iamrahulpanchal <rahulnpanchal50@gmail.com>
2020-10-01 15:12:43 +0530

    Creating Tasks with Postman

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('./db/mongoose');   // For Connecting to DB
 const Users = require('./models/users');
+const Tasks = require('./models/tasks');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,11 +11,19 @@ app.use(express.json());
 app.post('/users', (req, res) => {
     const user = new Users(req.body);
     user.save().then(() => {
-        res.send(user);
+        res.status(201).send(user);
     }).catch((err) => {
         res.status(400).send(err);
-        // res.send(err);
     })
+});
+
+app.post('/tasks', (req, res) => {
+    const task = new Tasks(req.body);
+    task.save().then(() => {
+        res.status(201).send(task);
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
 });
 
 app.listen(port, () =>{
