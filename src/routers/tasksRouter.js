4d39cc388abfa0e2260fe4fa48ad6a48135f3e65
iamrahulpanchal <rahulnpanchal50@gmail.com>
2020-10-03 01:37:02 +0530

    Updated Tasks Routes

--- src/routers/tasksRouter.js
+++ src/routers/tasksRouter.js
@@ -44,7 +44,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
     }
 });
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ['completed', 'description'];
     const isValidUpdate = updates.every((item) => {
@@ -60,24 +60,30 @@ router.patch('/tasks/:id', async (req, res) => {
     try {
         const id = req.params.id;
         const body = req.body;
-        const task = await Tasks.findById(id);
+        const task = await Tasks.findOne({
+            _id: id,
+            owner: req.user._id
+        });
+        if(!task){
+            return res.status(404).send();
+        }
         updates.forEach((field) => {
             task[field] = body[field];
         });
         await task.save();
-        if(!task){
-            return res.status(404).send();
-        }
         res.send(task);
     } catch (e) {
         res.status(400).send(e);
     }
 });
 
-router.delete('/tasks/:id', async (req, res) =>{
+router.delete('/tasks/:id', auth, async (req, res) =>{
     const id = req.params.id;
     try {
-        const task = await Tasks.findByIdAndDelete(id);
+        const task = await Tasks.findOneAndDelete({
+            _id: id,
+            owner: req.user._id
+        });
         if(!task){
             return res.status(404).send({
                 error: "Task Not Found!"
