623f38d19f30d4776bc84d1d59dfc2981951225d
iamrahulpanchal <rahulnpanchal50@gmail.com>
2020-10-03 01:21:52 +0530

    Read Task by ID with User Logged In

--- src/routers/tasksRouter.js
+++ src/routers/tasksRouter.js
@@ -26,10 +26,13 @@ router.get('/tasks', async (req, res) => {
     }
 });
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id;
     try {
-        const task = await Tasks.findById(_id);
+        const task = await Tasks.findOne({
+            _id: _id,
+            owner: req.user._id
+        });
         if(!task){
             return res.status(404).send();
         }
