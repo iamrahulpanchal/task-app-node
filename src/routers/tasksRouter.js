6d991b6fb31166b9eae8dc5c1399c50fb4e8c166
iamrahulpanchal <rahulnpanchal50@gmail.com>
2020-10-03 01:24:32 +0530

    Get All Tasks Related to User

--- src/routers/tasksRouter.js
+++ src/routers/tasksRouter.js
@@ -17,9 +17,11 @@ router.post('/tasks', auth, async (req, res) => {
     }
 });
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
     try {
-        const tasks = await Tasks.find({});
+        const tasks = await Tasks.find({
+            owner: req.user._id
+        });
         res.send(tasks);
     } catch (e) {
         res.status(500).send();
