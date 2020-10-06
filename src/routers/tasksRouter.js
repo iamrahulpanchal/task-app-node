f261c72ccf55710b3732a681a0ef8d30bbee7720
iamrahulpanchal <rahulnpanchal50@gmail.com>
2020-10-07 00:18:33 +0530

    Adding SKIP for going to next page

--- src/routers/tasksRouter.js
+++ src/routers/tasksRouter.js
@@ -27,7 +27,8 @@ router.get('/tasks', auth, async (req, res) => {
             path: 'tasks',
             match: match,
             options: {
-                limit: parseInt(req.query.limit)
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
             }
         }).execPopulate();
         res.send(req.user.tasks);
