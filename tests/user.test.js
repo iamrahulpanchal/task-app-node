d23e5695c3355b6436e4bf3c403e67822e000f8b
iamrahulpanchal <rahulnpanchal50@gmail.com>
2020-10-07 19:09:34 +0530

    Wiping DB before Creating User

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,17 @@
 const request = require('supertest');
 const app = require('../src/app');
+const Users = require('../src/models/usersModel');
+
+// Wipe DB before Each Test
+beforeEach(async () => {
+    await Users.deleteMany();
+});
 
 test('Signup a New User', async () => {
     await request(app).post('/users').send({
-        name: 'Rahul Testing',
-        email: 'rahul@example.com',
-        password: 'rahultesting',
-        age: 22
+        name: 'Rahul Panchal',
+        email: 'rahulnpanchal50@gmail.com',
+        password: 'rahul123',
+        age: 24
     }).expect(201);
 });
